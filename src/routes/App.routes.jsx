9e6d7d6a34c02d16a4361a2ba17a028e6e97819f
Adro82 <amendi82@gmail.com>
2023-03-08 10:19:57 +0100

    done

--- src/routes/App.routes.jsx
+++ src/routes/App.routes.jsx
@@ -5,10 +5,14 @@ import Events from "../pages/Events/Events"
 import Products from "../pages/Products/Products"
 import Recipes from "../pages/Recipes/Recipes"
 import AboutTempeh from "../pages/AboutTempeh/AboutTempeh"
-
+import NewProductPage from "../pages/NewProductPage/NewProductPage"
+import EditProductForm from "../components/EditProductForm/EditProductForm"
+import SingleProductCard from "../components/SingleProductCard/SingleProductCard"
+import UserListPage from "../pages/UserListPage/UserListPage"
+import LoginPage from "../pages/LoginPage/LoginPage"
+import SignupPage from "../pages/SignUpPage/SignUpPage"
 
 const AppRoutes = () => {
-
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
@@ -17,8 +21,14 @@ const AppRoutes = () => {
             <Route path="/Events" element={<Events />} />
             <Route path="/Products" element={<Products />} />
             <Route path="/Recipes" element={<Recipes />} />
-            {/* <Route path="/SignUp" element={<SignupPage />} />
-            <Route path="/Login" element={<LoginPage />} /> */}
+            <Route path="/NewProductPage" element={<NewProductPage />} />
+            <Route path="/products/edit/:id" element={<EditProductForm />} />
+            <Route path="/products/details/:id" element={<SingleProductCard />} />
+            <Route path="/appUsers" element={<UserListPage />} />
+            <Route path="/crearUsuario" element={<SignupPage />} />
+            <Route path="/iniciar-sesion" element={<LoginPage />} />
+
+            {/* <Route path="/Login" element={<LoginPage />} /> */}
             <Route path="*" element={<p>404</p>} />
         </Routes>
     )
