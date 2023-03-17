2845937a082bf2c49ae9759e028ce6da8956d6ab
aldidjamil <alditamadjamil@gmail.com>
2023-03-17 09:32:59 +0100

    done

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -1,6 +1,8 @@
 import RecipesList from "../../components/RecipesList/RecipesList"
 import { Link, useNavigate } from 'react-router-dom'
 import { Container, Button } from 'react-bootstrap'
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth.context"
 
 const Recipes = () => {
 
@@ -14,7 +16,7 @@ const Recipes = () => {
                     <h1>Recetas de BeanBased</h1>
                     <Button className="px-5 mx-5 justify-content-center" onClick={() => navigate(-1)} variant="outline-dark">Volver</Button>
                     <Link to={'/newRecipePage'}>
-                        <Button variant="outline-dark">Crear Recetas</Button>
+                        {user ? <Button variant="outline-dark">Crear Recetas</Button> : ''}
                     </Link>
                     <hr />
                     <RecipesList />
